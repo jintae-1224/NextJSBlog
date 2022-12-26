509c8f23cdb3bd064a4e2b16204d44747c6197c2
jintae-1224 <kab7210@naver.com>
2022-12-27 01:26:11 +0900

    ErrorBoundary

---
+++ blog/components/ErrorBoundary.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false, error: null, errorInfo: null }
+  }
+
+  static getDerivedStateFromError(error) {
+    console.log(`getDerivedStateFromError : ${error}`)
+    return { hasError: true }
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({ error, errorInfo })
+    console.log(`componentDidCatch : ${(error, errorInfo)}`)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      if (this.props.fallbackComponents) {
+        return this.props.fallbackComponents
+      }
+      return (
+        <div>
+          <h2>Error occured!!</h2>
+          <detail style={{ whiteSpace: 'pre-wrap' }}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+          </detail>
+        </div>
+      )
+    }
+
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
--- blog/pages/posts/[id].js
+++ blog/pages/posts/[id].js
@@ -5,6 +5,7 @@ import CodeBlock from '../../components/CodeBlock'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import utilStyles from '../../styles/utils.module.css'
 import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react'
 
 const Button = dynamic(() => import('../../components/Button'), {
   loading: () => <div>Loading...</div>,
@@ -32,24 +33,34 @@ const components = { Button, CodeBlock }
 
 export default function Post({ postData, pathname }) {
   const router = useRouter()
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    if (error) {
+      throw new Error('My Error')
+    }
+  }, [error])
 
   if (router.isFallback) {
     return <div>Loading</div>
   }
 
   return (
-    <article>
-      <h2>pathname : {pathname}</h2>
-      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-      <div className={utilStyles.lightText}>
-        <Date dateString={postData.date} />
-      </div>
-      {postData.contentHtml && (
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      )}
-      {postData.mdxSource && (
-        <MDXRemote {...postData.mdxSource} components={components} />
-      )}
-    </article>
+    <>
+      <button onClick={() => setError(true)}>Error</button>
+      <article>
+        <h2>pathname : {pathname}</h2>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        {postData.contentHtml && (
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        )}
+        {postData.mdxSource && (
+          <MDXRemote {...postData.mdxSource} components={components} />
+        )}
+      </article>
+    </>
   )
 }
