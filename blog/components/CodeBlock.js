ff0ca4fda477ecb9a21a6258f3e468e9b7f5ca8d
JinTae <kab7210@naver.com>
2022-12-26 10:51:38 +0900

    CodeBlock, CodeBlock Copy

--- blog/components/CodeBlock.js
+++ blog/components/CodeBlock.js
@@ -1,3 +1,36 @@
+import { useRef } from 'react'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { rainbow } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
+
+const CopyButton = ({ target }) => {
+  const handleCopy = async() => {
+    if(target) {
+      try{
+        await navigator.clipboard.writeText(target)
+        alert('copied')
+      }catch(error){
+        alert(`copy failed ${error}`)
+      }
+    }
+    
+  }
+  return (
+    <button
+      onClick={handleCopy}
+      className="absolute right-0.5 top-0.5 rounded-lg px-2 bg-white dark:text-gray-800"
+    >
+      copy
+    </button>
+  )
+}
+
 export default function CodeBlock({ children }) {
-  return <div>{children}</div>
+  return (
+    <div className="relative">
+      <CopyButton target={children}>Copy</CopyButton>
+      <SyntaxHighlighter showLineNumbers style={rainbow}>
+        {children}
+      </SyntaxHighlighter>
+    </div>
+  )
 }
--- blog/pages/posts/[id].js
+++ blog/pages/posts/[id].js
@@ -26,7 +26,7 @@ export async function getStaticProps({ params }) {
 
 const Button = ({ children }) => {
   return (
-    <button onClick={() => alert(`thanks to ${children}`)}>{children}</button>
+    <button className='bg-black dark:bg-white text-lg text-teal-200 dark:text-teal-700 rounded-lg px-5' onClick={() => alert(`thanks to ${children}`)}>{children}</button>
   )
 }
 
