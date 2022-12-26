52c9abce4dad5f36a45dbb785d8d8359ecba7118
jintae-1224 <kab7210@naver.com>
2022-12-26 22:16:07 +0900

    reportWebVitals

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -1,17 +1,22 @@
 import Layout from '../components/Layout'
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
-import { useState } from 'react';
-import {formatDistanceToNow } from 'date-fns';
+import { useState } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+
+export function reportWebVitals(metric) {
+  console.log(metric)
+}
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
+  const router = useRouter()
   const [visitedTime] = useState(new Date())
   // visitedTime은 새로고침 전까지 계속 유지됨
   return (
-    <Layout home={router.pathname==="/"}>
+    <Layout home={router.pathname === '/'}>
       <div>
-        visitedTime : {formatDistanceToNow(new Date(visitedTime), {
+        visitedTime :{' '}
+        {formatDistanceToNow(new Date(visitedTime), {
           addSuffix: true,
           includeSeconds: true,
         })}
